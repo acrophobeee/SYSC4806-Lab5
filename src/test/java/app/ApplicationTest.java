de8a1c4d83f0058060fcc35e499863e5c148b286
xinyuchen <nick.chenbca@gmail.com>
2021-02-12 09:38:35 -0500

    new commit

--- src/test/java/app/ApplicationTest.java
+++ src/test/java/app/ApplicationTest.java
@@ -6,24 +6,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.testng.annotations.Test;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-@WebMvcTest
-public class WebLayerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/buddy")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, World")));
     }
 }
